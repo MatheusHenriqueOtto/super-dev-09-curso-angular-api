3db5c9d5b75b7d7bc3263eff3c116fd8941308d6
Matheus Otto <12745@proway.treina>
2026-06-26 21:57:20 -0300

    Parei no meio da atividade

--- src/app/relatorios-financeiros/relatorio-financeiro-cadastrar/relatorio-financeiro-cadastrar.ts
+++ src/app/relatorios-financeiros/relatorio-financeiro-cadastrar/relatorio-financeiro-cadastrar.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RelatorioFinanceiroService } from '../../services/relatorio-financeiro.service';
+import { Router } from '@angular/router';
+import { RelatorioFinanceiroModel } from '../../models/relatorio-finaceiro.model';
 
 @Component({
   selector: 'app-relatorio-financeiro-cadastrar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './relatorio-financeiro-cadastrar.html',
   styleUrl: './relatorio-financeiro-cadastrar.scss',
 })
-export class RelatorioFinanceiroCadastrar {}
+export class RelatorioFinanceiroCadastrar {
+  private readonly relatorioFinanceiroService = inject(RelatorioFinanceiroService);
+  private readonly router = inject(Router)
+
+  relatorioFinanceiro = signal<RelatorioFinanceiroModel>({
+    id: 0,
+    titulo: "",
+    tipo: "",
+    valorTotal: 0,
+    dataEmissao: "",
+    responsavel: "",
+  });
+
+  salvar(): void {
+    this.relatorioFinanceiroService.cadastrar(this.relatorioFinanceiro()).subscribe({
+      next: relatorioFinanceiro => {
+        alert("Deu boa, o relatorio foi cadastrado!");
+        this.router.navigate(["relatorios-financeiros"]);
+      },
+      error: erro => {
+        console.error("Erro ao cadastrar o relatorio, deu "+ erro);
+        alert("Não deu boa");
+      }
+    })
+  }
+}
--- src/app/services/relatorio-financeiro.service.ts
+++ src/app/services/relatorio-financeiro.service.ts
@@ -1,6 +1,23 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { RelatorioFinanceiroModel } from '../models/relatorio-finaceiro.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RelatorioFinanceiroService {}
+export class RelatorioFinanceiroService {
+  private readonly http = inject(HttpClient);
+  private readonly baseUrl = `${environment.apiUrl}/api/v1/trabalho/relatorios-financeiros`;
+  
+  listar(): Observable<RelatorioFinanceiroModel[]> {
+    return this.http.get<RelatorioFinanceiroModel[]>(this.baseUrl);
+  }
+
+  cadastrar(relatorioFinanceiro: RelatorioFinanceiroModel): Observable<RelatorioFinanceiroModel> {
+    return this.http.post(this.baseUrl, relatorioFinanceiro);
+  } 
+
+
+}
